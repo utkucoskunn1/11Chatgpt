965edefb1e42e5e92e22c559a2bcaa4059e43c4a
UtkuCoskun <m.utkucoskun@icloud.com>
2024-07-18 03:14:38 +0300

    Initial Commit

---
+++ Themify/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Themify
+//
+//  Created by Utku on 18/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Themify/ThemifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThemifyApp.swift
+//  Themify
+//
+//  Created by Utku on 18/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ThemifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
